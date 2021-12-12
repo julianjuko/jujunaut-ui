da74ad27407179308542c698706bb485725a655c
Julian Anning <julian.anning@gmail.com>
2021-12-12 21:37:05 +1000

    Initial commit

---
+++ build/index.cjs.js
@@ -0,0 +1,16 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var React = require('react');
+
+function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+
+var React__default = /*#__PURE__*/_interopDefaultLegacy(React);
+
+var Button = function (props) {
+    return React__default["default"].createElement("div", null);
+};
+
+exports.Button = Button;
+//# sourceMappingURL=index.cjs.js.map
---
+++ build/index.esm.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+var Button = function (props) {
+    return React.createElement("div", null);
+};
+
+export { Button };
+//# sourceMappingURL=index.esm.js.map
