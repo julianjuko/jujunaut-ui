fa74d87ac4bcf6f293065a70724d93df6a9e0218
Julian Anning <julian.anning@gmail.com>
2022-01-15 13:58:26 +1000

    Export layout components

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 export * from './components/Button'
+export * from './components/Layout/Container'
+export * from './components/Layout/Row'
+export * from './components/Layout/Col'
 
 export { GlobalStyle } from './shared/style'
